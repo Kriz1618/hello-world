ddd0ec4bfdb16928f856c7734282137c19e87906
Kriz <krizmarles@gmail.com>
2018-02-02 22:35:22 -0500

    webpack configuration

---
+++ src/server/index.js
@@ -0,0 +1,35 @@
+// Dependencies
+import express from 'express';
+import webpack from 'webpack';
+import path from 'path';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import open from 'open';
+
+//webpack configuration
+import webpackConfig from '../webpack.config.dev';
+
+//Server Port
+const port = 3000;
+
+//Express App
+const app = express();
+
+//Webpack Compiler
+const webpackCompiler = webpack(webpackCompiler);
+
+//webpack middleware
+app.use(webpackDevMiddleware(webpackCompiler)); 
+app.use(webpackHotMiddleware(webpackCompiler)); 
+
+//sending all trafic to React
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+//Listen port
+app.listen(port, err => {
+    if(!err) {
+        open('http://localhost:${port}');
+    }
+})
